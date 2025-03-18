6914d9b67b347b51fdedea81a2ad063e5def1046
Yamin <141956369+yamin-401533@users.noreply.github.com>
2025-03-18 17:23:14 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './components/App.css';
 
 import AppHeader from './components/header';
 import AppHero from './components/hero';
