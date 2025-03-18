3a2da7508d17b8fb25485b4184a436d149d13f79
Yamin <141956369+yamin-401533@users.noreply.github.com>
2025-03-18 17:14:57 +0600

    Update fan.js

--- src/components/fan.js
+++ src/components/fan.js
@@ -8,7 +8,7 @@ import Nav from 'react-bootstrap/Nav';
 import Tab from 'react-bootstrap/Tab';
 import Spinner from 'react-bootstrap/Spinner';
 import { Heart, Pen, PaintBucket, Film, ExternalLink } from 'lucide-react';
-import './App.css';
+import './components/App.css';
 // Import images
 
 
