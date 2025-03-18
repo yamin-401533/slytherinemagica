e5fd7169bb1a745821085b0d20ee0deec65b6b1c
Yamin <141956369+yamin-401533@users.noreply.github.com>
2025-03-18 17:14:44 +0600

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,7 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import './App.css';
+import './components/App.css';
 
 
 
